93f832c5c1ae12442e2b926e927f628d0e950b1e
paolocalzone <paolocalzone@protonmail.com>
2022-11-03 13:06:40 +0100

    feat: add explore query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,33 @@
 import type { NextPage } from "next";
+import {
+  ExplorePublicationsDocument,
+  ExplorePublicationResult,
+} from "@/types/lens";
+import { useQuery } from "@apollo/client";
 import LoginButton from "@/components/LoginButton";
 
 const Home: NextPage = () => {
+  const { data, loading, error } = useQuery<{
+    explorePublications: ExplorePublicationResult;
+  }>(ExplorePublicationsDocument, {
+    variables: {
+      sortCriteria: "LATEST",
+      limit: 10,
+      metadata: {
+        mainContentFocus: ["VIDEO"],
+      },
+    },
+  });
+  const publications = data?.explorePublications.items;
+  console.log("DATA", data?.explorePublications);
   return (
     <div>
-      <LoginButton />
+      <>
+        <LoginButton />
+        {publications?.map((publication) => {
+          <div>{publication.metadata.media[0].original.url}</div>;
+        })}
+      </>
     </div>
   );
 };
