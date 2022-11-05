2d999dafa33367232c369e6dafc1fa34b07b7865
Natalie <natalieg@Natalies-MacBook-Air-3.local>
2022-11-05 19:06:05 +0200

    sidebar css

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -6,9 +6,10 @@ const LoginButton: FC = () => {
   const currentProfile = useAppStore((state) => state.currentProfile);
   return (
     <>
-      <div className="rounded-lg font-bold disabled:opacity-50 shadow-sm focus:ring-2 focus:ring-opacity-50 focus:ring-offset-1 outline-none">
+      <button className="cursor-pointer bg-white text-lg text-[#96de26] border-[1px] border-[#96de26] font-semibold px-6 py-3 rounded-md outline-none w-full mt-3 hover:text-[#25511f] hover:bg-[#96de26]"
+               type="button">
         {currentProfile ? currentProfile.handle : <LoginWallet />}
-      </div>
+      </button>
     </>
   );
 };
--- src/components/UI/Navbar.tsx
+++ src/components/UI/Navbar.tsx
@@ -1,4 +1,4 @@
-import logo from "@/images/logo.png";
+import logo from "@/images/Lenstoklogo.png";
 import Link from "next/link";
 import Image from "next/image";
 
