269748fdcfda46d31169347eb1f907ffeff9f1a2
driespindola <driespindola15@protonmail.com>
2022-11-06 09:33:24 -0300

    Revert "Update"
    This reverts commit da5a023f934d4476a24c2709d6ff16bac76ad8e3.

---
+++ src/components/Home.tsx
@@ -0,0 +1,25 @@
+import type { NextPage } from "next";
+
+import Navbar from "@/components/UI/Navbar";
+import Sidebar from "@/components/UI/Sidebar";
+import Explore from "@/components/Explore";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <div className="xl:w-[1200px] m-auto overflow-hidden h-[100vh]">
+        <Navbar />
+        <div className="flex gap-6 md:gap-20 ">
+          <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+            <Sidebar />
+          </div>
+          <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
+            <Explore />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,3 @@
-import {
-    ExplorePublicationsDocument,
-    ExplorePublicationResult,
-  } from "@/types/lens";
-  import type { Publication } from "@/types/lens";
-  import { useQuery } from "@apollo/client";
-  import VideoCard from "@/components/UI/VideoCard";
-  
-  const Home = () => {
-    const { data, loading, error } = useQuery<{
-      explorePublications: ExplorePublicationResult;
-    }>(ExplorePublicationsDocument, {
-      variables: {
-        request: {
-          sortCriteria: "TOP_COMMENTED",
-          publicationTypes: ["POST"],
-          limit: 10,
-          metadata: {
-            mainContentFocus: ["VIDEO"],
-          },
-        },
-      },
-    });
-    const publications = data?.explorePublications.items;
-    console.log("DATA", data?.explorePublications.items);
-    return (
-      <div>
-        {publications?.map((pub) => (
-          <VideoCard key={pub.id} publication={pub as Publication} />
-        ))}
-      </div>
-    );
-  };
-  
-  export default Home;
+import Home from "@/components/Home";
+
+export default Home;
