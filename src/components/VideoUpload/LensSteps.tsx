42b358e8631c4b1c422e5f30111b8af0c6a72e7a
paolocalzone <paolocalzone@protonmail.com>
2022-12-09 16:09:14 +0100

    fix: set LENSTOK_URL to vercel url

--- src/components/VideoUpload/LensSteps.tsx
+++ src/components/VideoUpload/LensSteps.tsx
@@ -152,7 +152,7 @@ const LensSteps = () => {
         ],
         appId: "lenstok",
       };
-      const response = await fetch(` http://localhost:3000/api/meta-to-ipfs`, {
+      const response = await fetch(` http://¨${LENSTOK_URL}/api/meta-to-ipfs`, {
         method: "POST",
         headers: { "Content-type": "application/json" },
         body: JSON.stringify(metadata),
