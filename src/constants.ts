eab3d9a076fea4ae1951e20956dc80e44e638533
Natalie <natalieg@Natalies-MacBook-Air-3.local>
2022-12-12 02:53:07 +0200

    oops polygon

--- src/constants.ts
+++ src/constants.ts
@@ -65,3 +65,7 @@ export const FREE_COLLECT_MODULE = IS_MAINNET
 
 export const MAINNET_DEFAULT_TOKEN =
   "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270";
+
+  export const POLYGONSCAN_URL = IS_MAINNET
+  ? "https://polygonscan.com"
+  : "https://mumbai.polygonscan.com";
