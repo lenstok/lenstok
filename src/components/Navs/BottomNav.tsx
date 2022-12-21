10418723babe1c45f86a762f8994fb76d9552694
Natalie <natalieg@Natalies-MacBook-Air-3.local>
2022-12-21 16:35:13 +0200

    mobile timeline switch

--- src/components/Navs/BottomNav.tsx
+++ src/components/Navs/BottomNav.tsx
@@ -2,17 +2,38 @@ import Link from 'next/link'
 import { IoMdAdd } from 'react-icons/io'
 import { useAppStore } from "src/store/app";
 import { toast } from "react-hot-toast";
+import SearchBarMobile from '../Search/SearchBarMobile';
+import { useState } from 'react';
 
 const BottomNav: React.FC = () => {
   const currentProfile = useAppStore((state) => state.currentProfile);
+  const [homePage, setHomePage] = useState(true);
 
   return (
     <div>
     <nav className="fixed bottom-0 left-0 right-0 h-[47px] rounded-t-md bg-emerald-700 z-10 flex items-center justify-around px-4 py-3 shadow-md">
     
-    <button className="text-white hover:text-gray-100 focus:outline-none focus:text-gray-100 border-gray-800">
+    { homePage ? (
+      <Link href='/latest'>
+        <button  
+        onClick= {() => {
+        {setHomePage(false);}
+        }} 
+       className="text-white hover:text-gray-100 focus:outline-none focus:text-gray-100 border-gray-800">
         <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
       </button>
+      </Link>
+    ) : (
+      <Link href='/'>
+        <button  
+        onClick= {() => {
+        {setHomePage(false);}
+        }} 
+       className="text-white hover:text-gray-100 focus:outline-none focus:text-gray-100 border-gray-800">
+        <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
+      </button>
+      </Link>
+      )}
 
       <Link href='/upload'>
        <button 
