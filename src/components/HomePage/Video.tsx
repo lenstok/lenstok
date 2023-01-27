833bb89ca55c3396391da007adf598367a135276
N44TS <96575678+N44TS@users.noreply.github.com>
2023-01-27 21:57:25 +0200

    Update Video.tsx

--- src/components/HomePage/Video.tsx
+++ src/components/HomePage/Video.tsx
@@ -59,7 +59,7 @@ const Video: FC<Props> = ({ publication }) => {
             loop
             muted
             autoPlay
-            // ref={videoRef}
+            ref={videoRef}
             src={getMedia(publication)}
             // className='lg:w-[400px] h-[300px] md:h-[400px] lg:h-[500px] w-[400px] rounded-2xl cursor-pointer bg-gray-100'
             className='lg:w-[410px] lg:h-[547px] md:h-[400px] md:w-[400px] h-[547px] w-full shadow-inner 
